8778b9bf55c20d2c8d891fee7b0ddf59f00518f0
ivartz <djloek@gmail.com>
2020-04-15 18:45:40 +0200

    modify scripts/analyze-medians-and-dice-scores.sh and scripts/compute-dice-between-rois.sh to make analyze and compute Dice between ground truth rois and uncorrected and corrected rois

--- scripts/.ipynb_checkpoints/analyze-medians-and-dice-scores-checkpoint.sh
+++ scripts/.ipynb_checkpoints/analyze-medians-and-dice-scores-checkpoint.sh
@@ -3,8 +3,6 @@
 
 correction_dir=$1
 
-run_evals=0
-
 # Gradient echo (e1) and Spin echo (e2)
 
 for (( i = 1 ; i < 3 ; i++ )) ; do
@@ -15,6 +13,14 @@ for (( i = 1 ; i < 3 ; i++ )) ; do
     readarray mni_medians_topup_files_arr_e${i} < <(find $correction_dir/EPI_applytopup -type d -name *e${i}_prep_topup_applytopup_postp_perf | xargs -I {} echo {}/mniroismedians.txt)
 
     readarray mni_medians_raw_files_arr_e${i} < <(find $correction_dir/EPI_raw_DSC -type d -name *e${i}_perf | xargs -I {} echo {}/mniroismedians.txt)
+
+    # MNI ROI dice files (ground truth)
+
+    readarray gt_epic_mni_dice_files_arr_e${i} < <(find $correction_dir/EPI_applyepic -type d -name *e${i}_applyepic_perf | xargs -I {} echo {}/mniroisgtepicdice.txt)
+
+    readarray gt_topup_mni_dice_files_arr_e${i} < <(find $correction_dir/EPI_applytopup -type d -name *e${i}_prep_topup_applytopup_postp_perf | xargs -I {} echo {}/mniroisgttopupdice.txt)
+
+    readarray gt_raw_mni_dice_files_arr_e${i} < <(find $correction_dir/EPI_raw_DSC -type d -name *e${i}_perf | xargs -I {} echo {}/mniroisgtrawdice.txt)
 
     # MNI ROI dice files
 
@@ -34,4 +40,5 @@ script=$(dirname $0)/wilcoxon-and-mni-rois-dice-analysis.py
 
 command="python $script --rawmedians ${mni_medians_raw_files_arr_e2[@]} --cormedians ${mni_medians_epic_files_arr_e2[@]} --dicescores ${raw_epic_mni_dice_files_arr_e2[@]}"
 
-eval $command
+echo $command
+#eval $command
--- scripts/compute-dice-between-rois.sh
+++ scripts/compute-dice-between-rois.sh
@@ -3,7 +3,7 @@
 
 correction_dir=$1
 
-run_evals=1
+run_evals=0
 
 script=$(dirname $0)/get-dice-between-rois.py
 
@@ -16,6 +16,11 @@ readarray rcbv_topup_files_arr_e1 < <(find $correction_dir/EPI_applytopup -type 
 
 readarray rcbv_raw_files_arr_e1 < <(find $correction_dir/EPI_raw_DSC -type d -name *e1_perf | xargs -I {} echo {}/Normalized_rCBV_map_-Leakage_corrected.nii)
 
+# Template ROIs in DSC space (ground truth)
+readarray mnigtrois_epic_files_arr_e1 < <(find $correction_dir/EPI_applyepic -type d -name gtrois_e1 | xargs -I file echo file/rlabels_Neuromorphometrics_lrmerged.nii)
+readarray mnigtrois_topup_files_arr_e1 < <(find $correction_dir/EPI_applytopup -type d -name gtrois_e1 | xargs -I file echo file/rlabels_Neuromorphometrics_lrmerged.nii)
+readarray mnigtrois_raw_files_arr_e1 < <(find $correction_dir/EPI_raw_DSC -type d -name gtrois_e1 | xargs -I file echo file/rlabels_Neuromorphometrics_lrmerged.nii)
+
 # Template ROIs in DSC space
 readarray mnirois_epic_files_arr_e1 < <(find $correction_dir/EPI_applyepic -type f -name ryinvapplied_e1*)
 readarray mnirois_topup_files_arr_e1 < <(find $correction_dir/EPI_applytopup -type f -name ryinvapplied_e1*)
@@ -27,6 +32,42 @@ readarray tumorrois_topup_files_arr_e1 < <(find $correction_dir/EPI_applytopup -
 readarray tumorrois_raw_files_arr_e1 < <(find $correction_dir/EPI_raw_DSC -type f -name re1_tumor_segments.nii)
 
 for (( i = 0 ; i < ${#rcbv_epic_files_arr_e1[@]} ; i++ )) ; do
+    
+    echo "---- GT TO EPIC ----"
+    cbvdir=$(dirname ${rcbv_epic_files_arr_e1[i]})
+    
+    mniroiscommand="python $script --rois1 ${mnigtrois_epic_files_arr_e1[i]} --rois2 ${mnirois_epic_files_arr_e1[i]} > $cbvdir/mniroisgtepicdice.txt"
+    
+    echo $mniroiscommand
+    
+    if [ $run_evals == 1 ] ; then
+        echo $mniroiscommand > $cbvdir/mniroisgtepicdicecommand.txt
+        eval $mniroiscommand
+    fi
+    
+    echo "---- GT TO TOPUP ----"
+    cbvdir=$(dirname ${rcbv_topup_files_arr_e1[i]})
+    
+    mniroiscommand="python $script --rois1 ${mnigtrois_topup_files_arr_e1[i]} --rois2 ${mnirois_topup_files_arr_e1[i]} > $cbvdir/mniroisgttopupdice.txt"
+    
+    echo $mniroiscommand
+    
+    if [ $run_evals == 1 ] ; then
+        echo $mniroiscommand > $cbvdir/mniroisgttopupdicecommand.txt
+        eval $mniroiscommand
+    fi
+    
+    echo "---- GT TO RAW ----"
+    cbvdir=$(dirname ${rcbv_raw_files_arr_e1[i]})
+    
+    mniroiscommand="python $script --rois1 ${mnigtrois_raw_files_arr_e1[i]} --rois2 ${mnirois_raw_files_arr_e1[i]} > $cbvdir/mniroisgtrawdice.txt"
+    
+    echo $mniroiscommand
+    
+    if [ $run_evals == 1 ] ; then
+        echo $mniroiscommand > $cbvdir/mniroisgtrawdicecommand.txt
+        eval $mniroiscommand
+    fi
     
     echo "---- RAW TO EPIC ----"
     cbvdir=$(dirname ${rcbv_epic_files_arr_e1[i]})
@@ -70,6 +111,11 @@ readarray rcbv_topup_files_arr_e2 < <(find $correction_dir/EPI_applytopup -type 
 
 readarray rcbv_raw_files_arr_e2 < <(find $correction_dir/EPI_raw_DSC -type d -name *e2_perf | xargs -I {} echo {}/Normalized_rCBV_map_-Leakage_corrected.nii)
 
+# Template ROIs in DSC space (ground truth)
+readarray mnigtrois_epic_files_arr_e2 < <(find $correction_dir/EPI_applyepic -type d -name gtrois_e2 | xargs -I file echo file/rlabels_Neuromorphometrics_lrmerged.nii)
+readarray mnigtrois_topup_files_arr_e2 < <(find $correction_dir/EPI_applytopup -type d -name gtrois_e2 | xargs -I file echo file/rlabels_Neuromorphometrics_lrmerged.nii)
+readarray mnigtrois_raw_files_arr_e2 < <(find $correction_dir/EPI_raw_DSC -type d -name gtrois_e2 | xargs -I file echo file/rlabels_Neuromorphometrics_lrmerged.nii)
+
 # Template ROIs in DSC space
 readarray mnirois_epic_files_arr_e2 < <(find $correction_dir/EPI_applyepic -type f -name ryinvapplied_e2*)
 readarray mnirois_topup_files_arr_e2 < <(find $correction_dir/EPI_applytopup -type f -name ryinvapplied_e2*)
@@ -81,7 +127,43 @@ readarray tumorrois_topup_files_arr_e2 < <(find $correction_dir/EPI_applytopup -
 readarray tumorrois_raw_files_arr_e2 < <(find $correction_dir/EPI_raw_DSC -type f -name re2_tumor_segments.nii)
 
 for (( i = 0 ; i < ${#rcbv_epic_files_arr_e2[@]} ; i++ )) ; do
+
+    echo "---- GT TO EPIC ----"
+    cbvdir=$(dirname ${rcbv_epic_files_arr_e2[i]})
     
+    mniroiscommand="python $script --rois1 ${mnigtrois_epic_files_arr_e2[i]} --rois2 ${mnirois_epic_files_arr_e2[i]} > $cbvdir/mniroisgtepicdice.txt"
+    
+    echo $mniroiscommand
+    
+    if [ $run_evals == 1 ] ; then
+        echo $mniroiscommand > $cbvdir/mniroisgtepicdicecommand.txt
+        eval $mniroiscommand
+    fi
+    
+    echo "---- GT TO TOPUP ----"
+    cbvdir=$(dirname ${rcbv_topup_files_arr_e2[i]})
+    
+    mniroiscommand="python $script --rois1 ${mnigtrois_topup_files_arr_e2[i]} --rois2 ${mnirois_topup_files_arr_e2[i]} > $cbvdir/mniroisgttopupdice.txt"
+    
+    echo $mniroiscommand
+    
+    if [ $run_evals == 1 ] ; then
+        echo $mniroiscommand > $cbvdir/mniroisgttopupdicecommand.txt
+        eval $mniroiscommand
+    fi
+    
+    echo "---- GT TO RAW ----"
+    cbvdir=$(dirname ${rcbv_raw_files_arr_e2[i]})
+    
+    mniroiscommand="python $script --rois1 ${mnigtrois_raw_files_arr_e2[i]} --rois2 ${mnirois_raw_files_arr_e2[i]} > $cbvdir/mniroisgtrawdice.txt"
+    
+    echo $mniroiscommand
+    
+    if [ $run_evals == 1 ] ; then
+        echo $mniroiscommand > $cbvdir/mniroisgtrawdicecommand.txt
+        eval $mniroiscommand
+    fi
+
     echo "---- RAW TO EPIC ----"
     cbvdir=$(dirname ${rcbv_epic_files_arr_e2[i]})
     
